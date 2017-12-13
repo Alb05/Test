47ff129318d72d822a25d6e43b9983f4f5fac6e7
atefkhalifa <atef.khalifa@libero.it>
2017-12-13 16:04:42 +0100

    commit 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,11 +24,11 @@ import { LoginComponent } from './components/login/login.component';
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
       { path: 'home', component: HomeComponent },
       { path: 'ajax', component: AjaxComponent },
       { path: 'login', component: LoginComponent },
-      { path: '**', redirectTo: 'home' }
+      { path: '**', redirectTo: 'login' }
     ])
   ],
   providers: [],
